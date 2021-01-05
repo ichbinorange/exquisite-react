43f4b0cda6bfe58ca0d7de306fac70de4395820a
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2021-01-05 14:06:03 -0800

    built six text input fields

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -3,17 +3,55 @@ import PropTypes from 'prop-types';
 
 import './PlayerSubmissionForm.css';
 
-const PlayerSubmissionForm = () => {
+const PlayerSubmissionForm = (props) => {
+  const [formFields, setFormField] = useState(props.field)
+
+  // Event handlers
+  const onInputChange = (event) => {
+    const newFormFields = {
+      ...formFields
+    }
+    newFormFields[event.target.name] = event.target.value
+    setFormField(newFormFields)
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    props.submissionCallback(formFields)
+
+    setFormField({
+      adjective: '',
+      noun: '',
+      adverb: '',
+      verb: '',
+    })
+  }
+
+  const inputContent = props.fields.map( (field, i) => {
+    if (field.key) {
+      return <input
+        key={ i }
+        placeholder={ field.placeholder }
+        // value={ [field.key] }
+        onChange={onInputChange}
+        type="text"
+      />;
+    } else {
+      return field;
+    }
+  })
+
   return (
     <div className="PlayerSubmissionForm">
-      <h3>Player Submission Form for Player #{  }</h3>
+      <h3>Player Submission Form for Player #{ props.index }</h3>
 
-      <form className="PlayerSubmissionForm__form" >
+      <form className="PlayerSubmissionForm__form" onSubmit={onFormSubmit} >
 
         <div className="PlayerSubmissionForm__poem-inputs">
 
           {
             // Put your form inputs here... We've put in one below as an example
+            inputContent
           }
           <input
             placeholder="hm..."
